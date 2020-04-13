6170025ec5591dd4b8e1b5ac4a9eee296fa15cea
lamido <kinglamido@gmail.com>
2020-04-13 16:23:27 +0100

    bg-3 fix

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 const express = require('express');
-// const convert = require('xml-js');
+const morgan = require('morgan');
+const fs = require('fs');
+const path = require('path');
+const convert = require('xml-js');
 const covid19ImpactEstimator = require('./covid-19/estimator');
 const responseType = require('./utils/index');
 
@@ -8,6 +11,35 @@ const createApp = () => {
 
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
+
+  app.use(morgan('tiny'));
+
+  app.use(
+    morgan(
+      (tokens, req, res) => {
+        let resTime = parseInt(
+          tokens['response-time'](req, res),
+          10
+        ).toString();
+
+        if (resTime.length === 1) resTime = `0${resTime}`;
+
+        const log = [
+          tokens.method(req, res),
+          tokens.url(req, res),
+          tokens.status(req, res),
+          resTime
+        ].join('\t\t');
+
+        return `${log}ms`;
+      },
+      {
+        stream: fs.createWriteStream(path.join(__dirname, 'logs.txt'), {
+          flags: 'a'
+        })
+      }
+    )
+  );
 
   app.post('/api/v1/on-covid-19', async (req, res) => {
     try {
@@ -32,6 +64,14 @@ const createApp = () => {
 
       const data = await covid19ImpactEstimator(req.body);
 
+      if (param === 'xml') {
+        const options = { compact: true, ignoreComment: true, spaces: 4 };
+
+        const xmlData = await convert.json2xml(data, options);
+
+        return res.status(200).send(xmlData);
+      }
+
       return res.status(200).json({
         ...data
       });
@@ -43,28 +83,23 @@ const createApp = () => {
     }
   });
 
-  // app.post('/api/v1/on-covid-19/xml', async (req, res) => {
-  //   try {
-  //     const data = await covid19ImpactEstimator(req.body);
+  app.get('/api/v1/on-covid-19/logs', async (req, res) => {
+    try {
+      res.setHeader('Content-Type', 'text/plain');
 
-  //     const options = { compact: true, ignoreComment: true, spaces: 4 };
-
-  //     const xmlData = await convert.json2xml(data, options);
-
-  //     res.setHeader('Content-Type', 'application/xml');
-
-  //     return res.status(200).type('application/xml').send(..data);
-  //   } catch (error) {
-  //     return res.status(400).json({
-  //       status: 'error',
-  //       error
-  //     });
-  //   }
-  // });
-
-  app.get('/api/v1/on-covid-19/logs', (req, res) => {
-    res.setHeader('Content-Type', 'text/plain');
-    res.send('logs');
+      return fs.readFile('./logs.txt', (err, data) => {
+        if (err) {
+          res.send(err);
+          return;
+        }
+        res.status(200).send(data);
+      });
+    } catch (error) {
+      return res.status(400).json({
+        status: 'error',
+        error
+      });
+    }
   });
 
   return app;
